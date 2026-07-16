8c66086aaf0f81bc6c62c8e250083a81d9ba2e71
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-16 13:53:16 -0300

    Create projetos.js

---
+++ projetos.js
@@ -0,0 +1,11 @@
+if (listaProjetosBrutos){
+// Lógica de Autogeração: Varre a lista bruta criando IDs numéricos aleatórios que nunca se repetem
+const listaProjetos = listaProjetosBrutos.map((projeto, index) => {
+  const carimboTempo = Date.now().toString().slice(-4);
+  const numeroAleatorio = Math.floor(100 + Math.random() * 900);
+  return {
+    ...projeto,
+    id: parseInt(`${index}${carimboTempo}${numeroAleatorio}`)
+  };
+});
+}
