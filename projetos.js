fd7d24df8af0e9c47ce5d7869ead995c4ca05bae
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-16 16:42:06 -0300

    Update projetos.js

--- projetos.js
+++ projetos.js
@@ -1,5 +1,6 @@
 function verificarLista(lista) {  return lista && lista.length > 0 ? lista : [];     }
-let listaProjetosBrutodfs = verificarLista(listaProjetosBrutos) ?? [];
+let dlistaProjetosBrutodfs = verificarLista(listaProjetosBrutos);
+let listaProjetosBrutodfs = dlistaProjetosBrutodfs ?? [];
 
 // Lógica de Autogeração: Varre a lista bruta criando IDs numéricos aleatórios que nunca se repetem
 const listaProjetos = listaProjetosBrutodfs.map((projeto, index) => {
