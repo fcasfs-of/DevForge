df53a4262e3c36cb970e7cabf4bc5a60f677e4f1
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-16 16:26:37 -0300

    Update projetos.js

--- projetos.js
+++ projetos.js
@@ -1,10 +1,9 @@
 function garantirLista(lista) {    return lista !== undefined ? lista : [];   }
 
-let listaProjetosBrutos = garantirLista(listaProjetosBrutos);
-
+let listaProjetosBrutodfs = garantirLista(listaProjetosBrutos);
 
 // Lógica de Autogeração: Varre a lista bruta criando IDs numéricos aleatórios que nunca se repetem
-const listaProjetos = listaProjetosBrutos.map((projeto, index) => {
+const listaProjetos = listaProjetosBrutodfs.map((projeto, index) => {
   const carimboTempo = Date.now().toString().slice(-4);
   const numeroAleatorio = Math.floor(100 + Math.random() * 900);
   return {
