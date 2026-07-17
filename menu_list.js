8472bcf6571f227f40339dad04455765e65cece7
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-07-17 14:01:04 -0300

    Update menu_list.js

--- menu_list.js
+++ menu_list.js
@@ -1,15 +1,16 @@
 const minhaListaDeMenus = [
-   { 
+   {    
+    textPt: 'Menu Principal', 
+    textEn: 'Main Menu', 
+    link: '#', 
+    svg: '',
+    submenus: [
+  { 
     textPt: 'Sobre', 
     textEn: 'About', 
     link: '/info?pg=sobre', 
     svg: '<svg width="18" height="18" viewBox="0 0 96 96" fill="currentColor"><g><path d="M66,84H54V42a5.9966,5.9966,0,0,0-6-6H36a6,6,0,0,0,0,12h6V84H30a6,6,0,0,0,0,12H66a6,6,0,0,0,0-12Z"/><path d="M48,24A12,12,0,1,0,36,12,12.0119,12.0119,0,0,0,48,24Z"/></g></svg>'
-  },   { 
-    textPt: 'Fale Conosco', 
-    textEn: 'Contact Us', 
-    link: '/contato', 
-    svg: '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6zm3.519 0L12 11.671 18.481 6H5.52zM20 7.329l-7.341 6.424a1 1 0 0 1-1.318 0L4 7.329V18h16V7.329z"/></svg>'
-  },   { 
+  },  { 
     textPt: 'Privacidade', 
     textEn: 'Privacy', 
     link: '/info?pg=privacidade', 
@@ -19,10 +20,25 @@ const minhaListaDeMenus = [
     textEn: 'Terms', 
     link: '/info?pg=termos', 
     svg: '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M7,13v7a1,1,0,0,0,1,1H20a1,1,0,0,0,1-1V4a1,1,0,0,0-1-1H8A1,1,0,0,0,7,4V7" style="fill: none; stroke: rgb(0, 0, 0); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path><path d="M3.29,7.69l1.4-1.4a1,1,0,0,1,1.4,0L11,11.2V14H8.2L3.29,9.09A1,1,0,0,1,3.29,7.69ZM17,17H14" style="fill: none; stroke: rgb(44, 169, 188); stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"></path></svg>'
-  },   { 
+  }  
+ ]
+  },  {    
+    textPt: 'Suporte e Atendimento', 
+    textEn: 'Suport and Customer Service', 
+    link: '#', 
+    svg: '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M2,21h8a1,1,0,0,0,0-2H3.071A7.011,7.011,0,0,1,10,13a5.044,5.044,0,1,0-3.377-1.337A9.01,9.01,0,0,0,1,20,1,1,0,0,0,2,21ZM10,5A3,3,0,1,1,7,8,3,3,0,0,1,10,5Zm13,8.5v5a.5.5,0,0,1-.5.5h-1v2L19,19H14.5a.5.5,0,0,1-.5-.5v-5a.5.5,0,0,1,.5-.5h8A.5.5,0,0,1,23,13.5Z"/></svg>',
+    submenus: [
+  { 
     textPt: 'Perguntas Frequentes', 
     textEn: 'Frequently Asked Questions', 
     link: '/faq', 
     svg: '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12,1A11,11,0,1,0,23,12,11.013,11.013,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9.011,9.011,0,0,1,12,21Zm1-4.5v2H11v-2Zm3-7a3.984,3.984,0,0,1-1.5,3.122A3.862,3.862,0,0,0,13.063,15H11.031a5.813,5.813,0,0,1,2.219-3.936A2,2,0,0,0,13.1,7.832a2.057,2.057,0,0,0-2-.14A1.939,1.939,0,0,0,10,9.5,1,1,0,0,1,8,9.5V9.5a3.909,3.909,0,0,1,2.319-3.647,4.061,4.061,0,0,1,3.889.315A4,4,0,0,1,16,9.5Z"/></svg>'
+  },   { 
+    textPt: 'Fale Conosco', 
+    textEn: 'Contact Us', 
+    link: '/contato', 
+    svg: '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6zm3.519 0L12 11.671 18.481 6H5.52zM20 7.329l-7.341 6.424a1 1 0 0 1-1.318 0L4 7.329V18h16V7.329z"/></svg>'
   }
+    ] 
+   }
 ];
