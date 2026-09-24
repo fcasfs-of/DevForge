70f570ae7c616940687a1b677e554d5a48f4b1bf
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-24 09:05:04 -0300

    Update plists.js

--- plists.js
+++ plists.js
@@ -8,11 +8,11 @@ var listaProjetosBrutos = [
   "iconeSvg": "<path d='M13.08 5.94C11.84 5.57 10.61 5.2 9.37 4.83C9.24 6.32 9.11 7.81 8.97 9.3C8.98 11.85 8.99 14.39 8.99 16.94C7.46 17.79 5.92 18.65 4.38 19.5C4.2 17.99 4.03 16.47 3.86 14.96C4.11 10.88 3.03 0.5 9.95 1.83C10.99 3.2 12.04 4.57 13.08 5.94Z' fill='#448fb3' fill-rule='evenodd' stroke='#448fb3' stroke-width='0.25' stroke-linejoin='round'></path><path d='M15.31 13.06C16.83 13.87 18.35 14.69 19.87 15.5C18.6 16.66 17.32 17.82 16.05 18.98C10 20.02 9.23 24.87 2.17 21.86C1.56 19.8 2.3 16.47 3.86 14.96C4.03 16.47 4.2 17.99 4.38 19.5C5.92 18.65 7.46 17.79 8.99 16.94C11.1 15.64 13.2 14.35 15.31 13.06Z' fill='#e4348b' fill-rule='evenodd' stroke='#e4348b' stroke-width='0.25' stroke-linejoin='round'></path><path d='M13.08 5.94C17.3 9.29 24.65 10.97 22.72 17.83C20.5 18.22 18.28 18.6 16.05 18.98C17.32 17.82 18.6 16.66 19.87 15.5C18.35 14.69 16.83 13.87 15.31 13.06C13.2 11.81 11.09 10.56 8.97 9.3C9.11 7.81 9.24 6.32 9.37 4.83C10.61 5.2 11.84 5.57 13.08 5.94Z' fill='#eea832' fill-rule='evenodd' stroke='#eea832' stroke-width='0.25' stroke-linejoin='round'></path>",
   "pt": {
     "titulo": "LightBox: Token",
-    "descricao": "O token remove a marca d'água do LightBox no rodapé."
+    "descricao": "O token remove a marca d'água do \"LightBox - Conteúdos Diversos\" no rodapé."
   },
   "en": {
     "titulo": "LightBox: Token",
-    "descricao": "The token removes the LightBox watermark from the footer."
+    "descricao": "The token removes the \"LightBox - Diverse Contents\" watermark from the footer."
   },
   "tecnologias": [
     { "nome": "JavaScript",   "icone": "<path d='M24 0H0v24h24V0zM11.7 18.2c-.4.9-1.2 1.5-2.5 1.5-1.7 0-2.7-1.1-3-2.3h1.7c.2.6.6 1 1.3 1 .7 0 1.1-.3 1.1-.9V11h1.7v6.9.3zm5.6 1.5c-1.4 0-2.5-.8-3-2h1.6c.3.5.7.8 1.4.8.6 0 1-.3 1-.7s-.3-.6-1-.9l-.5-.2c-1.3-.5-2.1-1.2-2.1-2.5 0-1.4 1.1-2.4 2.6-2.4 1.3 0 2.2.6 2.7 1.7h-1.5c-.3-.4-.6-.6-1.1-.6-.6 0-.9.3-.9.7s.3.5.9.7l.6.3c1.4.6 2.1 1.2 2.1 2.5-.1 1.5-1.2 2.6-2.8 2.6z' fill='currentColor'/>"  },
