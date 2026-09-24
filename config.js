305740578b2af4d4fe1f5bf51b210fe70470def3
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-09-24 09:14:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.sysConfig={whatsapp:"558393308741",email:"fcasfs.of@cloud-fs.net",chaveIdioma:"premium-lang",chaveTema:"premium-theme",itensPorPagina:12,chaveCarrinho:"premium-cart-items"};
+window.sysConfig={whatsapp:"5583933008741",email:"fcasfs.of@cloud-fs.net",chaveIdioma:"premium-lang",chaveTema:"premium-theme",itensPorPagina:12,chaveCarrinho:"premium-cart-items"};
